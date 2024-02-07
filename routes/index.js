a4703f689ee3899cac056b624f1f02023d75678b
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2024-02-06 21:59:28 -0700

    Changed the endpoint that delivers the swagger api documentation

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,14 @@ const router = express.Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
 
+router.get('/', (req, res) => {
+    res.send('Welcome to my Bug Tracker API!<br><br>Add "/api-docs" to the end of the URL to view all available API requests.')
+});
+
+
 router.use('/bugs', require('./bugs'));
 
-router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(swaggerDocument));
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
