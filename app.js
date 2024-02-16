7f23baa6db2d795288cbe064d830b2964e258ce4
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2024-02-16 15:41:14 -0700

    Added error handling to the getAll and getSingle functions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const mongodb = require('./db/connect');
+const bodyParser = require('body-parser');
 
 app.use(express.json())
    .use(bodyParser.json())
--- controllers/bugs.js
+++ controllers/bugs.js
@@ -1,22 +1,36 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('bugs').find();
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
-};
+const getAll = (req, res) => {
+    mongodb
+      .getDb()
+      .db()
+      .collection('bugs')
+      .find()
+      .toArray((err, lists) => {
+        if (err) {
+          res.status(400).json({ message: err});
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+      })
+    };
   
-  const getSingle = async (req, res) => {
-    const bugId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection('bugs').find({ _id: bugId });
-    result.toArray().then((lists) => {
+const getSingle = (req, res) => {
+  const bugId = new ObjectId(req.params.id);
+  mongodb
+    .getDb()
+    .db()
+    .collection('bugs')
+    .find({ _id: bugId})
+    .toArray((err, lists) => {
+      if (err) {
+        res.status(400).json({ message: err});
+      }
       res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists[0]);
-    });
-};
+      res.status(200).json(result[0]);
+    })
+  };
 
 const createBug = async (req, res) => {
     const bug = {
@@ -57,7 +71,7 @@ const updateBug = async (req, res) => {
   if (response.modifiedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    res.status(500).json(response.error || 'Some error occurred while updating the bug report.');
   }
 };
 
@@ -68,7 +82,7 @@ const deleteBug = async (req, res) => {
   if (response.deletedCount > 0) {
     res.status(200).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    res.status(500).json(response.error || 'Some error occurred while deleting the bug report.');
   }
 };
 
