c4bcb9b5c42e303d64c8ee75b5595ec6b7a1da66
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2024-02-24 21:29:04 -0700

    Added more detail to the 401 that is sent to a user when trying to access unauthorized content

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/connect');
 const passport = require('passport');
 
 function isLoggedIn(req, res, next) {
-    req.user ? next() : res.sendStatus(401);
+    req.user ? next() : res.status(401).send("Unauthorized: You need to be logged in to access this endpoint.");;
 }
 
 module.exports = {
